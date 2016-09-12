599dba5a97b8679534ff857c8a23b453a519fdf8
charlesrt <charlesrt.digitaldwp@gmail.com>
2016-09-12 15:40:05 +0100

    Find GP progress

--- app/routes.js
+++ app/routes.js
@@ -24,7 +24,7 @@ router.get('/give-your-address/confirm-address', function (req, res) {
 
 router.post('/find-your-gp-doctor/select-gp', function (req, res) {
 
-  if (req.body.selectPractice == "false"){
+  if (req.body.gpPractice == "false"){
     res.redirect("/find-your-gp-doctor/manual-gp");
   } else {
     res.render('find-your-gp-doctor/select-gp');
@@ -32,20 +32,12 @@ router.post('/find-your-gp-doctor/select-gp', function (req, res) {
 
 });
 
-router.get('/find-your-gp-doctor/confirm-gp', function (req, res) {
+router.post('/find-your-gp-doctor/confirm-gp', function (req, res) {
 
-  var selectGP = req.query.selectGP;
-      selectPractice = req.body.practice;
-      console.log(req.body);
-
-
-  if (selectGP == "false"){
+  if (req.body.gpName == "false"){
     res.redirect("/find-your-gp-doctor/manual-gp");
   } else {
-    res.render('find-your-gp-doctor/confirm-gp', {
-      'practice' : selectPractice,
-      'gp' : selectGP
-    });
+    res.render('find-your-gp-doctor/confirm-gp');
   }
 
 });
